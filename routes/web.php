fe85b3ee0178d731bd53b674ad83ab38004d2cd2
i.shved <igor.mail.mobile@gmail.com>
2022-12-26 16:20:44 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+//Route::get('/', function () { return view('welcome'); });
 Route::get('/', [MainPageController::class, 'index']);
