c8ad0c921b2169cb85de3d3ace907dd1ff4bced2
Igor Shved <igor.mail.mobile@gmail.com>
2023-01-19 18:20:39 +0200

    main.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,4 @@ const app = createApp({});
 app.component('main-block', require('./components/MainBlock.vue').default);
 app.component('room-block', require('./components/RoomBlock.vue').default);
 app.mount('#heater');
+
