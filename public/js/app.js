df253805b1b4106d28ed963ac7607a4c43ae6396
Igor Shved <igor.mail.mobile@gmail.com>
2023-01-19 18:51:18 +0200

    main.css

--- public/js/app.js
+++ public/js/app.js
@@ -19745,6 +19745,10 @@ app.mount('#heater');
 
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 
+// try {
+//     require('bootstrap');
+// } catch (e) {}
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () { return view('welcome'); });
 Route::get('/', [MainPageController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
